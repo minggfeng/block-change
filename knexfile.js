a4c1c73c9b7462c0df7214a08e7bca57454a6a20
Yzhang1994 <yzhang1994@gmail.com>
2017-07-08 16:26:39 -0700

    update db migrations

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,16 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'block_change',
+      database: 'block-change',
       user: 'postgres',
       password: ''
     },
     pool: {
       min: 2,
       max: 10
+    },
+    migrations: {
+      directory: 'db/migrations'
     }
   },
 
