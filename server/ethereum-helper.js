7b25e9ceedb0f44b6b56afe8b3dbff3068b9f375
kluwena <karelluwena@gmail.com>
2017-07-08 20:44:16 -0700

    move ethereum helper to ethereum server

--- server/ethereum-helper.js
+++ server/ethereum-helper.js
@@ -1,16 +1,3 @@
-var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-
-/*addr is wallet address*/
-var balance = (addr) => {
-  return web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
-}
-
-var sendTransaction = (addr1, addr2) => {
-  return web3.eth.sendTransaction({
-    from: addr1, to: addr2, value: web3.toWei(10, 'ether'), gasLimit: 21000, gasPrice: 2000000000
-  })
-}
 
 
 
