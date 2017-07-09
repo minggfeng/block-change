31df828bd7ed96d3b0cad89395b8b77a0bc2a082
Ming <kakacuna@gmail.com>
2017-07-09 04:46:08 -0700

    Updated transaction history

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -12,26 +12,13 @@ import {
 class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      home: false
-    }
-    this.changeProp = this.changeProp.bind(this);
-  }
-
-  changeProp(key, val) {
-    this.setState({
-      [key]: val,
-    });
   }
 
   render() {
-    if (this.state.home) {
-      return <Redirect to="/"/>
-    }
     return (
       <Toolbar>
         <ToolbarGroup>
-          <ToolbarTitle onClick={e => this.changeProp('home', !this.state.home)} text="Block Change" />
+          <ToolbarTitle text="Block Change" />
           <FontIcon className="muidocs-icon-custom-sort" />
         </ToolbarGroup>
         <ToolbarGroup>
