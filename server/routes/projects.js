45029ee9ab72a8b328c42fc99b78671f5da5c3ca
Carlo P. Las Marias <carlol@Carlo-MBP.local>
2017-07-08 23:45:18 -0700

    linter fixes

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,16 +1,17 @@
 const express = require('express');
+
 const router = express.Router();
 const models = require('../../db/models');
-const middleware = require('../middleware');
+// const middleware = require('../middleware');
 
 router.route('/create')
 .post((req, res) => {
   models.Project.forge(req.body).save()
-  .then(project => {
+  .then((project) => {
     res.status(201).send(project.id);
   })
-  .catch(err => {
-    res.status(500).send(false);
+  .catch((err) => {
+    res.status(500).send(err);
   });
 });
 
