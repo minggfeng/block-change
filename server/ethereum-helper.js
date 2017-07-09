4e86e8ab4ee7d2f61ee89c6bda70e74e38f082d3
kluwena <karelluwena@gmail.com>
2017-07-08 20:08:24 -0700

    create sendTransaction function

--- server/ethereum-helper.js
+++ server/ethereum-helper.js
@@ -5,3 +5,8 @@ var balance = (acct) => {
   return web3.fromWei(web3.eth.getBalance(acct), 'ether').toNumber();
 }
 
+var sendTransaction = (acct1, acct2) => {
+  return web3.eth.sendTransaction({
+    from: acct1, to: acct2, value: web3.toWei(10, 'ether'), gasLimit: 21000, gasPrice: 2000000000
+  })
+}
