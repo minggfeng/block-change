cce14eb8bf652b782d12fbd4d279f39cb3a54d42
carlolm <carlol@Carlo-MBP.local>
2017-07-09 02:59:25 -0700

    linter

--- ethereum/helpers/accounts.js
+++ ethereum/helpers/accounts.js
@@ -1,6 +1,5 @@
-
 const Web3 = require('web3');
-const EthTx = require('ethereumjs-tx');
+
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 /* addr is wallet address*/
@@ -8,10 +7,10 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const balance = addr => web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
 
 const sendTransaction = (amount, addr1, addr2) => web3.eth.sendTransaction({
-  from: addr1, 
-  to: addr2, 
-  value: web3.toWei(amount, 'ether'), 
-  gasLimit: 21000, 
+  from: addr1,
+  to: addr2,
+  value: web3.toWei(amount, 'ether'),
+  gasLimit: 21000,
   gasPrice: 2000000000,
 });
 
@@ -23,5 +22,5 @@ module.exports = {
   balance,
   sendTransaction,
   getTransaction,
-  getTransactionCount
+  getTransactionCount,
 };
