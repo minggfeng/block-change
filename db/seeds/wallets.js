adf678bf025afe860863c9c1f7274b5b321ac354
carlolm <carlol@gmail.com>
2017-07-09 08:55:49 -0700

    updated seeding

--- db/seeds/wallets.js
+++ db/seeds/wallets.js
@@ -1,6 +1,40 @@
 const ethAddresses = require('../../ethereum/db/wallet_addresses');
 
+const projects = require('../example.js');
+
 exports.seed = (knex, Promise) => knex('user_wallets').del()
   .then(() => knex('project_wallets').del())
+  .then(() => knex('profiles').del())
+  .then(() => knex('projects').del())
   .then(() => knex('user_wallets').insert(ethAddresses.userWallets))
-  .then(() => knex('project_wallets').insert(ethAddresses.projectWallets));
+  .then(() => knex('project_wallets').insert(ethAddresses.projectWallets))
+  .then(() => {
+    const wallets = ethAddresses.userWallets.slice(1, 11);
+    const profiles = [];
+    wallets.forEach((x, index) => {
+      profiles.push({
+        id: index,
+        email: `f@l${index}`,
+        profile_wallet: x.wallet_address,
+        password: 'p',
+        debit: '100000',
+      });
+    });
+    return knex('profiles').insert(profiles);
+  })
+  .then(() => {
+    const wallets = ethAddresses.projectWallets.slice(1, 11);
+    const projectsArray = [];
+    wallets.forEach((x, index) => {
+      projectsArray.push({
+        id: index,
+        profile_id: index + 1,
+        title: projects[index % projects.length].title,
+        description: projects[index % projects.length].description,
+        goal: 10000 * (Math.random() * 10 | 0),
+        project_wallet: x.wallet_address,
+        image: projects[index % projects.length].image,
+      });
+    });
+    return knex('projects').insert(projectsArray);
+  });
