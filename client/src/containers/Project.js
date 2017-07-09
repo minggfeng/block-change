f0efad085ecd136c25c230ab0362429916131f71
Ming <kakacuna@gmail.com>
2017-07-09 02:44:47 -0700

    Css change for projects

--- client/src/containers/Project.js
+++ client/src/containers/Project.js
@@ -6,6 +6,13 @@ import { toggleDonate, setProjectInFocus } from '../actions';
 import Header from '../components/Header';
 import Donate from '../components/Donate';
 import axios from 'axios';
+import LinearProgress from 'material-ui/LinearProgress';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+  margin: 12,
+};
 
 class Project extends Component {
   constructor(props) {
@@ -15,18 +22,30 @@ class Project extends Component {
   render() {
     const { id, title, description, goal, image } = this.props.projects[this.props.index];
     return (
-      <div>
+      <div style={{width: "100%"}}>
         <Header />
-        <h1>{title}</h1>
-        <img src={image} alt={id} width={500}/>
-        <p><b>Description:</b> {description}</p>
-        <div>Amount Raised: {goal}</div>
-        <div>Goal: {goal}</div>
-        <RaisedButton label="Donate" onTouchTap={this.props.toggleDonate}/>
-        <Donate
-          {...this.props}
-          project={this.props.projects[this.props.index]}
-        />
+        <div style={{margin: "auto", width: "65%", padding: "20px"}}>
+          <Card>
+            <CardMedia>
+              <img src={image}/>
+            </CardMedia>
+
+            <CardTitle title={title}/>
+            <div style={{ padding: "10px" }}>
+              Goal: {goal}
+            </div>
+            <div style={{ padding: "10px" }}>
+              Amount Raised: 5000
+            </div>
+            <div style={{ padding: "10px" }}>  
+              <LinearProgress mode="determinate" value={(5000/goal) * 100} />
+            </div>
+
+            <CardText>{description}</CardText>
+
+            <RaisedButton label="Donate" primary={true} style={style} ></RaisedButton>
+          </Card>
+        </div>
       </div>
     );
   }
@@ -40,6 +59,7 @@ const mapStateToProps = (state) => {
   };
 };
 
+<<<<<<< HEAD
 const matchDispatchToProps = (dispatch) => {
   return bindActionCreators({
     toggleDonate, setProjectInFocus,
@@ -47,3 +67,6 @@ const matchDispatchToProps = (dispatch) => {
 }
 
 export default connect(mapStateToProps, matchDispatchToProps)(Project);
+=======
+export default connect(mapStateToProps)(Project);
+>>>>>>> Css change for projects
