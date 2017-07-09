767438184f44749fe005c1870ac79bcb8b7b5fca
kluwena <karelluwena@gmail.com>
2017-07-09 02:03:40 -0700

    add get transaction and transaction count

--- ethereum/helpers/accounts.js
+++ ethereum/helpers/accounts.js
@@ -8,10 +8,20 @@ const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 const balance = addr => web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
 
 const sendTransaction = (amount, addr1, addr2) => web3.eth.sendTransaction({
-  from: addr1, to: addr2, value: web3.toWei(amount, 'ether'), gasLimit: 21000, gasPrice: 2000000000,
+  from: addr1, 
+  to: addr2, 
+  value: web3.toWei(amount, 'ether'), 
+  gasLimit: 21000, 
+  gasPrice: 2000000000,
 });
+
+const getTransaction = txHash => web3.eth.getTransaction(txHash);
+
+const getTransactionCount = addr => web3.eth.getTransactionCount(addr);
 
 module.exports = {
   balance,
   sendTransaction,
+  getTransaction,
+  getTransactionCount
 };
