ec539572977bc3eeabc59a518bc6d48de5abac53
carlolm <carlol@gmail.com>
2017-07-09 04:16:03 -0700

    clean up comments

--- ethereum/helpers/index.js
+++ ethereum/helpers/index.js
@@ -1,10 +1,8 @@
-
 const Web3 = require('web3');
+
 const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-/* addr is wallet address*/
-
-const balance = addr => web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
+const getBalance = addr => web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
 
 const sendTransaction = (amount, addr1, addr2) => web3.eth.sendTransaction({
   from: addr1,
@@ -19,7 +17,7 @@ const getTransaction = txHash => web3.eth.getTransaction(txHash);
 const getTransactionCount = addr => web3.eth.getTransactionCount(addr);
 
 module.exports = {
-  balance,
+  getBalance,
   sendTransaction,
   getTransaction,
   getTransactionCount,
