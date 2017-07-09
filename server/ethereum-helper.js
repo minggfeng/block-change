69c821c489dc0d5bc09d3233d48346cb5541d7ee
kluwena <karelluwena@gmail.com>
2017-07-08 20:22:53 -0700

    add naming fix to helper

--- server/ethereum-helper.js
+++ server/ethereum-helper.js
@@ -1,12 +1,14 @@
 var Web3 = require('web3');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-var balance = (acct) => {
-  return web3.fromWei(web3.eth.getBalance(acct), 'ether').toNumber();
+/*addr is wallet address*/
+var balance = (addr) => {
+  return web3.fromWei(web3.eth.getBalance(addr), 'ether').toNumber();
 }
 
-var sendTransaction = (acct1, acct2) => {
+var sendTransaction = (addr1, addr2) => {
   return web3.eth.sendTransaction({
-    from: acct1, to: acct2, value: web3.toWei(10, 'ether'), gasLimit: 21000, gasPrice: 2000000000
+    from: addr1, to: addr2, value: web3.toWei(10, 'ether'), gasLimit: 21000, gasPrice: 2000000000
   })
 }
+
